93cdc1b80d8e46b446a67a48ffd9447a6a504a09
Micik <vladimir.micinskiy@gmail.com>
2016-04-21 00:13:14 +0300

    fix include

--- console.php
+++ console.php
@@ -3,5 +3,5 @@
 define('ROOT', dirname(__FILE__).'/');
 define('PIXODO', dirname(__FILE__).'/Pixodo/');
 define('APP', dirname(__FILE__).'/application/');
-include PIXODO.'framework.php';
+require_once PIXODO.'framework.php';
 App::gi()->console();
